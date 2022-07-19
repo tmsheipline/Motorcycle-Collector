81314c5488ba1e7c21157690793eb91a92aba256
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-19 17:57:35 -0400

    Full CRUD finished

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Motorcycle(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.make
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'motorcycle_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
-
-
-# Add the Cat class & list and view function below the imports
-class Motorcycle:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, make, model, year, color):
-    self.make = make
-    self.model = model
-    self.year = year
-    self.color = color
-
-motorcycles = [
-  Motorcycle('Indian', 'Scout', 2017, 'Cream'),
-]
+from django.views.generic.edit import CreateView
+from .models import Motorcycle
 
 # Create your views here.
 def home(request):
@@ -22,4 +12,22 @@ def about(request):
   return render(request, 'about.html')
 
 def motorcycles_index(request):
+  motorcycles = Motorcycle.objects.all()
   return render(request, 'motorcycles/index.html', { 'motorcycles': motorcycles })
+
+def motorcycles_detail(request, motorcycle_id):
+  motorcycle = Motorcycle.objects.get(id=motorcycle_id)
+  return render(request, 'motorcycles/detail.html', { 'motorcycle': motorcycle })
+
+class MotorcycleCreate(CreateView):
+  model = Motorcycle
+  fields = '__all__'
+  success_url = '/motorcycles/'
+
+class MotorcycleUpdate(UpdateView):
+  model = Motorcycle
+  fields = '__all__'
+
+class MotorcycleDelete(DeleteView):
+  model = Motorcycle
+  success_url = '/motorcycles/'
