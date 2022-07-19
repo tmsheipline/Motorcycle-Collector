ffbca2fbb72e3d921a08a8331541196d6639b8de
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-19 16:41:44 -0400

    Color change added

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Add the Cat class & list and view function below the imports
+class Motorcycle:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, make, model, year, color):
+    self.make = make
+    self.model = model
+    self.year = year
+    self.color = color
+
+motorcycles = [
+  Motorcycle('Indian', 'Scout', 2017, 'Cream'),
+]
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Motorcycle Collector</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def motorcycles_index(request):
+  return render(request, 'motorcycles/index.html', { 'motorcycles': motorcycles })
